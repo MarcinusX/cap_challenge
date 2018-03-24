ccbb5dc30e0d86d9ea3b469dce75ec9780c68236
Marcin Szalek <marcin.szalek1@gmail.com>
2018-03-24 11:11:07 +0100

    Added fab on slivers

--- lib/widgets/challenges/challenge_details_page.dart
+++ lib/widgets/challenges/challenge_details_page.dart
@@ -20,58 +20,131 @@ List<Bottle> bottleCollection = [
   new Bottle(BottleName.COCA_COLA_ZERO, Capacity.CAN_300, 50),
 ];
 
-class ChallengeDetailsPage extends StatelessWidget {
+class ChallengeDetailsPage extends StatefulWidget {
   final Challenge challenge;
-
-  double _rowHeight = 32.0;
 
   ChallengeDetailsPage(this.challenge);
 
   @override
+  ChallengeDetailsPageState createState() {
+    return new ChallengeDetailsPageState();
+  }
+}
+
+class ChallengeDetailsPageState extends State<ChallengeDetailsPage> {
+  final double _rowHeight = 32.0;
+
+  ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = new ScrollController();
+    _scrollController.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new CustomScrollView(
-        slivers: <Widget>[
-          _buildSliverAppBar(),
-          new SliverList(
-            delegate: new SliverChildListDelegate(
-              <Widget>[
-                new FloatingActionButton(onPressed: () {}),
-                _buildConfirmButtonOrLabel(bottleCollection, challenge),
-                new Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: new Row(
-                    children: <Widget>[
-                      new Text("Poziom trudności:  "),
-                      buildDifficultyIndicator(challenge),
-                    ],
-                  ),
+      body: new Stack(
+        children: <Widget>[
+          new CustomScrollView(
+            controller: _scrollController,
+            slivers: <Widget>[
+              _buildSliverAppBar(),
+              new SliverList(
+                delegate: new SliverChildListDelegate(
+                  <Widget>[
+                    _buildConfirmButtonOrLabel(
+                        bottleCollection, widget.challenge),
+                    new Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: new Row(
+                        children: <Widget>[
+                          new Text("Poziom trudności:  "),
+                          buildDifficultyIndicator(widget.challenge),
+                        ],
+                      ),
+                    ),
+                    new Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16.0, vertical: 8.0),
+                      child: new Row(
+                        children: <Widget>[
+                          new Text("Nagroda:  "),
+                          getRewardView(widget.challenge),
+                        ],
+                      ),
+                    ),
+                  ]..addAll(widget.challenge.requirements.keys.map((bottle) {
+                    return _buildRequirementRow(
+                      context,
+                      bottle,
+                      widget.challenge.requirements[bottle],
+                      bottleCollection
+                          .where((btl) => btl == bottle)
+                          .length,
+                    );
+                  }).toList())..addAll(
+                      widget.challenge.requirements.keys.map((bottle) {
+                        return _buildRequirementRow(
+                          context,
+                          bottle,
+                          widget.challenge.requirements[bottle],
+                          bottleCollection
+                              .where((btl) => btl == bottle)
+                              .length,
+                        );
+                      }).toList()),
                 ),
-                new Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 16.0, vertical: 8.0),
-                  child: new Row(
-                    children: <Widget>[
-                      new Text("Nagroda:  "),
-                      getRewardView(challenge),
-                    ],
-                  ),
-                ),
-              ]
-                ..addAll(challenge.requirements.keys.map((bottle) {
-                  return _buildRequirementRow(
-                    context,
-                    bottle,
-                    challenge.requirements[bottle],
-                    bottleCollection
-                        .where((btl) => btl == bottle)
-                        .length,
-                  );
-                }).toList()),
-            ),
+              ),
+            ],
           ),
+          _buildFab(),
         ],
       ),
+    );
+  }
+
+  Widget _buildFab() {
+    final defaultTop = 256.0 - 4.0;
+    final defaultSize = 56.0;
+    final defaultRight = 16.0;
+    double right = defaultRight;
+    double top = defaultTop;
+    double margin = 96.0;
+    double size = defaultSize;
+
+    if (_scrollController.hasClients) {
+      top = top - _scrollController.offset;
+      if (_scrollController.offset < defaultTop - margin) {
+        size = size;
+      } else if (_scrollController.offset < defaultTop - margin / 2) {
+        size = defaultTop - margin / 2.0 - _scrollController.offset;
+        top += (defaultSize - size) / 2.0;
+        right += (defaultSize - size) / 2.0;
+      } else {
+        size = 0.0;
+      }
+    }
+    return new Positioned(
+      child: new Container(
+        height: size,
+        width: size,
+        child: new FloatingActionButton(
+          heroTag: null,
+          onPressed: () {},
+          child: new Icon(Icons.check, size: (size) / 2.0,),
+        ),
+      ),
+      top: top,
+      right: right,
     );
   }
 
@@ -139,7 +212,7 @@ class ChallengeDetailsPage extends StatelessWidget {
         child: new Image.asset(
           "images/bottle_filled.png",
           height: _rowHeight,
-            ),
+        ),
       ),
     ).toList();
     List<Widget> emptyBottles = new Iterable.generate(
@@ -163,16 +236,16 @@ class ChallengeDetailsPage extends StatelessWidget {
       expandedHeight: 256.0,
       pinned: true,
       flexibleSpace: new FlexibleSpaceBar(
-        title: new Text(challenge.name),
+        title: new Text(widget.challenge.name),
         background: new Container(
           color: Colors.white,
           child: new Hero(
-            tag: "challenge_image_${challenge.name}",
+            tag: "challenge_image_${widget.challenge.name}",
             child: new Stack(
               fit: StackFit.expand,
               children: <Widget>[
                 new Image.network(
-                  challenge.photoUrl,
+                  widget.challenge.photoUrl,
                   height: 256.0,
                   fit: BoxFit.cover,
                 ),
