1c966ea7e73054ab777065ba5c98176a17dfed8d
Marcin Szalek <marcin.szalek1@gmail.com>
2018-04-09 11:17:02 +0200

    Added progress indicator in cap

--- lib/widgets/code_cap.dart
+++ lib/widgets/code_cap.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+import 'dart:math' as math;
+
 import 'package:cap_challenge/logic/http_service.dart' show sendBottleCode;
 import 'package:cap_challenge/models/add_code_result.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 //import 'package:qrcode_reader/QRCodeReader.dart';
 
 class CodeCap extends StatefulWidget {
@@ -10,19 +14,32 @@ class CodeCap extends StatefulWidget {
   }
 }
 
-class CodeCapState extends State<CodeCap> {
+class CodeCapState extends State<CodeCap> with TickerProviderStateMixin {
   TextEditingController _textController;
+  AnimationController _animationController;
+  Animation _animation;
   bool _shouldShowSubmitButton = false;
 
   @override
   void initState() {
     super.initState();
+    _animationController = new AnimationController(
+        duration: const Duration(seconds: 1), vsync: this);
+    _animation = new Tween<double>(begin: 0.0, end: 2 * math.pi)
+        .animate(_animationController);
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _animationController.reset();
+        _animationController.forward();
+      }
+    });
     _textController = new TextEditingController();
   }
 
   @override
   void dispose() {
     _textController.dispose();
+    _animationController.dispose();
     super.dispose();
   }
 
@@ -39,30 +56,6 @@ class CodeCapState extends State<CodeCap> {
     );
   }
 
-//
-//  Positioned _buildQRCodeFab(BuildContext context) {
-//    return new Positioned(
-//      left: 16.0,
-//      bottom: 72.0,
-//      child: new FloatingActionButton(
-//        onPressed: () {
-//          new QRCodeReader().scan().then((String qrCode) {
-//            if (qrCode != null) {
-//              new Future.delayed(const Duration(milliseconds: 500), () {
-//                Navigator.of(context).pop(new ScannedQRCodeResult(qrCode));
-//              });
-//            }
-//          });
-//        },
-//        child: new Image.asset(
-//          "images/qr_code.png",
-//          width: 32.0,
-//          color: Colors.white,
-//        ),
-//      ),
-//    );
-//  }
-
   Hero _buildCap() {
     return new Hero(
       tag: "fab-cap",
@@ -71,69 +64,93 @@ class CodeCapState extends State<CodeCap> {
         width: 300.0,
         child: new Stack(
           children: <Widget>[
-            new Image.asset(
-              "images/cap.png",
-              fit: BoxFit.fill,
-            ),
-            new Center(
-              child: new Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 68.0),
-                child: new TextField(
-                  autocorrect: false,
-                  maxLength: 14,
-                  decoration:
-                  new InputDecoration(labelText: "Kod spod nakrętki"),
-                  controller: _textController,
-                  onChanged: (text) =>
-                      setState(
-                              () =>
-                          _shouldShowSubmitButton = text.length == 14),
-                ),
-              ),
-            ),
-            new Opacity(
-              opacity: _shouldShowSubmitButton ? 1.0 : 0.0,
-              child: new Align(
-                alignment: Alignment.topCenter,
-                child: new Padding(
-                  padding: const EdgeInsets.only(top: 72.0),
-                  child: new RaisedButton(
-                    shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(16.0),
-                    ),
-                    onPressed: () =>
-                        sendBottleCode(_textController.text).then((response) {
-                          if (response.statusCode == 200) {
-                            Navigator
-                                .of(context)
-                                .pop(new AddedCodeResult(isOk: true));
-                          } else {
-                            showDialog(
-                              context: context,
-                              child: new AlertDialog(
-                                title: new Text("Ups..."),
-                                content: new Text("coś poszło nie tak"),
-                                actions: <Widget>[
-                                  new FlatButton(
-                                    onPressed: () =>
-                                        Navigator.of(context).pop(),
-                                    child: new Text("Zamknij"),
-                                  )
-                                ],
-                              ),
-                            );
-                          }
-                        }),
-                    color: Colors.red,
-                    child: new Text(
-                      "WYŚLIJ KOD",
-                      style: new TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-              ),
-            ),
+            _buildCapImage(),
+            _buildInput(),
+            _buildSubmitButton(),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCapImage() {
+    return new AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) =>
+      new Transform(
+        alignment: Alignment.center,
+        transform: new Matrix4.identity()
+          ..rotateZ(_animation.value),
+        child: new Image.asset(
+          "images/cap.png",
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSubmitButton() {
+    if (!_shouldShowSubmitButton) {
+      return new Container();
+    }
+    return new Align(
+      alignment: Alignment.topCenter,
+      child: new Padding(
+        padding: const EdgeInsets.only(top: 72.0),
+        child: new RaisedButton(
+          shape: new RoundedRectangleBorder(
+            borderRadius: new BorderRadius.circular(16.0),
+          ),
+          onPressed: () =>
+              _sendBottleCode(_textController.text).then(_handleSubmitResponse),
+          color: Colors.red,
+          child: new Text(
+            "WYŚLIJ KOD",
+            style: new TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<Response> _sendBottleCode(String code) async {
+    _animationController.forward();
+    Response response = await sendBottleCode(code);
+    _animationController.stop();
+    return response;
+  }
+
+  _handleSubmitResponse(Response response) {
+    if (response.statusCode == 200) {
+      Navigator.of(context).pop(new AddedCodeResult(isOk: true));
+    } else {
+      showDialog(
+        context: context,
+        child: new AlertDialog(
+          title: new Text("Ups..."),
+          content: new Text("coś poszło nie tak"),
+          actions: <Widget>[
+            new FlatButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: new Text("Zamknij"),
+            )
+          ],
+        ),
+      );
+    }
+  }
+
+  Center _buildInput() {
+    return new Center(
+      child: new Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 68.0),
+        child: new TextField(
+          autocorrect: false,
+          maxLength: 14,
+          decoration: new InputDecoration(labelText: "Kod spod nakrętki"),
+          controller: _textController,
+          onChanged: (text) =>
+              setState(() => _shouldShowSubmitButton = text.length == 14),
         ),
       ),
     );
