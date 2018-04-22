11e30fce68f69f2c88cb373bc5b0c40976a503f1
Marcin Szalek <marcin.szalek1@gmail.com>
2018-04-22 13:36:49 +0200

    Added points to functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -19,6 +19,13 @@ const sizes = {
     '5': '_2L'
 };
 
+const pointSheet = {
+    '1': 50,
+    '2': 100,
+    '3': 150,
+    '4': 300,
+    '5': 500,
+};
 
 export const OnCreateFunction = functions.auth.user().onCreate((event) => {
     return admin.database().ref('users/' + event.data.uid).set({
@@ -31,28 +38,36 @@ export const AddCapCode = functions.https.onRequest((request, response) => {
     const uid = request.get("uid");
 
     if (request.method === 'POST') {
-        const type = types[request.get("code").charAt(0).toLowerCase()];
-        const size = sizes[request.get("code").charAt(1).toLowerCase()];
+        const typeChar = request.get("code").charAt(0).toLowerCase();
+        const sizeChar = request.get("code").charAt(1).toLowerCase();
+
+        const type = types[typeChar];
+        const size = sizes[sizeChar];
 
         if (type === null || size === null) {
             response.status(400).send("Invalid code");
-            return;
+            return null;
         }
 
-        const path = 'users/' + uid + '/bottles/' + type + size;
-        let promise = admin.database().ref(path).once('value', (snapshot) => {
-            let quantity = 0;
-            if (snapshot.exists()) {
-                quantity = snapshot.val();
+        const path = 'users/' + uid;//+ '/bottles/' + type + size;
+        return admin.database().ref(path).once('value', (snapshot) => {
+            const val = snapshot.val();
+            let bottleQuantity = val.bottles[type + size];
+            if (bottleQuantity === null) {
+                bottleQuantity = 0;
             }
-            admin.database().ref(path).set(quantity + 1).then((value => {
-                response.status(200).send();
-            }), (value) => {
-                response.status(500).send();
-            });
+            let points = 0;
+            if (snapshot.hasChild('points')) {
+                points = val.points;
+            }
+            return Promise.all([
+                admin.database().ref(path + '/bottles/' + type + size).update(bottleQuantity + 1),
+                admin.database().ref(path + '/points').update(points + pointSheet[sizeChar])]
+            ).then((value) => response.status(200).send());
         });
     } else {
         response.status(404).send();
+        return null;
     }
 
 });
